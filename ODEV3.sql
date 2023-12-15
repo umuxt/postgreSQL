2daa15a300767bb2ea0c7c3d190a264b1f0dff44
umuxt <99925754+umuxt@users.noreply.github.com>
2023-12-16 02:32:40 +0300

    ODEV3
    this is the solution of 4 scenario of ODEV3

---
+++ ODEV3.sql
@@ -0,0 +1,16 @@
+--scenario 1
+SELECT country FROM country 
+WHERE country LIKE 'A%a';
+
+--scenario 2
+SELECT country FROM country
+WHERE LENGTH(country) >= 6 AND country LIKE '%n';
+
+--scenario 3
+SELECT title FROM film
+WHERE title ILIKE '%T%T%T%T%'
+
+--scenario 4
+SELECT * FROM film
+WHERE title LIKE 'C%' AND LENGTH(title) > 90 AND rental_rate = 2.99
+
